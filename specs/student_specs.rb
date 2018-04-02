3e9436a8b4a04cdda2b827e3356e2db14b45ef17
Paul Kelly <paul.kelly20@gmail.com>
2018-04-02 16:25:10 +0100

    First exercise completed

---
+++ specs/student_specs.rb
@@ -0,0 +1,38 @@
+require('minitest/autorun')
+require_relative('../student.rb')
+
+class TestStudent < Minitest::Test
+  def setup()
+    @student = Student.new("Paul", 5)
+
+  end
+
+  def test_name
+
+    assert_equal("Paul", @student.name)
+  end
+
+  def test_cohort
+
+    assert_equal(5, @student.cohort)
+    end
+
+    def test_set_name
+      @student.set_name("Dave")
+      assert_equal("Dave", @student.name)
+    end
+
+    def test_set_cohort
+      @student.set_cohort(6)
+      assert_equal(6, @student.cohort)
+    end
+
+    def test_student_talk
+      @student.can_talk()
+      assert_equal("i can talk!", @student.can_talk)
+    end
+
+    def test_favourite_language
+      assert_equal("I love Ruby", @student.student_favourite_language("Ruby"))
+    end
+end
---
+++ student.rb
@@ -0,0 +1,35 @@
+class Student
+
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def name()
+    return @name
+  end
+
+  def cohort()
+    return @cohort
+  end
+
+  def set_name(name)
+    @name = name
+  end
+
+  def set_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def can_talk()
+  return  "i can talk!"
+
+  end
+
+
+   def student_favourite_language(name)
+
+    return "I love " + name
+     end
+
+end
