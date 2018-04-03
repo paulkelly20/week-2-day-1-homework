701a9af7f649c1e7233e56b9b529edb57821b6ab
Paul Kelly <paul.kelly20@gmail.com>
2018-04-03 09:47:35 +0100

    Refactored question 1 on extension

--- library.rb
+++ library.rb
@@ -6,22 +6,22 @@ class Library
 
   end
 
-  def rental_books()
+  # def rental_books()
+  #   return @rental_books
+  # end
+
+  def book()
     return @rental_books
   end
 
-  # def rental_details()
-  #   return @rental_details
+  # def book(name)
+  #   for name in @rental_books
+  #   if @rental_books == name
+  #      return rental_details
+  #
+  #
+  #    end
   # end
-
-  def book(name)
-    for name in @rental_books
-    if @rental_books == name
-       return rental_details
-
-
-     end
-  end
-  
-  end
+  #
+  # end
 end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -3,20 +3,20 @@ require_relative('../library.rb')
 
 class TestLibrary < Minitest::Test
   def setup()
-    @rental_books = Library.new [{
+    @rental_books = Library.new([{
       title: "lord_of_the_rings" ,
       rental_details: {
        student_name: "Jeff",
        date: "01/12/16"
-      }
-    }]
-  #   [{
-  #   title: "Jaws" ,
-  #   rental_details: {
-  #    student_name: "Paul",
-  #    date: "25/12/16"
-  #   }
-  # }]
+     }
+    } ,
+  {
+    title: "Jaws" ,
+    rental_details: {
+     student_name: "Paul",
+     date: "25/12/16"
+    }
+  }])
   end
 
   def test_book
@@ -26,7 +26,13 @@ class TestLibrary < Minitest::Test
        student_name: "Jeff",
        date: "01/12/16"
       }
-    }], @rental_books.book("lord_of_the_rings"))
+    } , {
+      title: "Jaws" ,
+      rental_details: {
+       student_name: "Paul",
+       date: "25/12/16"
+      }
+    }] , @rental_books.book())
 
   end
 end
